bfe42f4ea762704eded5ec40d00b24b9af72d4e2
MuushyRoom <ronancuaresma10@gmail.com>
2025-01-17 19:59:14 +0800

    Game fixed and working properly - Changed alert to text results - Future features: guess win streak, sound cues, and code cleanup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,59 +1,69 @@
 let isVariableChanged = false;
 let isNewGame = true;
 let numberRandom = null;
+let computerText1;
+let computerText;
 
-console.log("RANDOM NUMBER GENERATED "+numberRandom);
+console.log("RANDOM NUMBER GENERATED " + numberRandom);
 
-function getRand(){
+function getRand() {
+  let customRange = parseInt(slider.value);
+  console.log("CHECK FOR CUSTOM RANGE SLIDER " + customRange);
+  computerText1 = document.getElementById("results").textContent =
+      "Random Number Generated";
 
-    let customRange = parseInt(slider.value);
-    console.log("CHECK FOR CUSTOM RANGE SLIDER "+customRange);
-    alert("Computer is Thinking...");
-   
-if(numberRandom == null) {
-    alert("Enter your guess");
-         numberRandom = Math.floor((Math.random() * customRange) + 1);
-        compNumber = numberRandom;
-       console.log("RANDOM NUMBER GENERATED "+numberRandom);
+  if (numberRandom == null) {
+    computerText = document.getElementById("results1").textContent =
+    "You can now enter your guess!";
+    numberRandom = Math.floor(Math.random() * customRange + 1);
+    compNumber = numberRandom;
+    console.log("RANDOM NUMBER GENERATED " + numberRandom);
     checkGuess(compNumber);
-   
-
-}else{
+  } else {
     checkGuess(compNumber);
+  }
 }
 
-}
-//THANK GOD FOR CHATGPT
-
-function isChanged(){
-    alert("Computer is Thinking...");
+function isChanged() {
+  //   alert("Computer is Thinking...");
+ 
+  computerText1 = document.getElementById("results").textContent =
+    " Welcome to the Guess the Number";
+    computerText = document.getElementById("results1").textContent =
+    "New Number Generated!";
   numberRandom = null;
-  
 }
 
-    function checkGuess(compNumber){
-      
-        let valueGet = document.getElementById("guessInput");
-        let userGuess = parseInt(valueGet.value);
-   
+function checkGuess(compNumber) {
+  let valueGet = document.getElementById("guessInput");
+  let userGuess = parseInt(valueGet.value);
 
-        if(compNumber < 3){
-
-            alert("Wong Guess!");
-        }else if(userGuess > compNumber){
-           //   alert(`User Guess: ${userGuess} is greater than Computer Number`); 
-              alert("Guess Lower!");
-            }else if(userGuess < compNumber){
-             // alert(`User Guess: ${userGuess} is less than Computer Number`); 
-              alert("Guess Higher!");
-            }else if(userGuess == compNumber){
-            //   alert(`User Guess: ${userGuess} is correct! Computer Number`); 
-              alert("Your Guess is correct!");
-              alert("Guess another number!!");
-              numberRandom = null;
-                 }
-                          
-    }
-
-
+  if (userGuess > compNumber) {
+    computerText = document.getElementById("results").textContent =
+    "YOUR GUESS IS WRONG!";
+    computerText1 = document.getElementById("results1").textContent =
+      "Guess Lower!";
     
+  } else if (userGuess < compNumber) {
+    computerText = document.getElementById("results").textContent =
+      "YOUR GUESS IS WRONG!";
+    computerText1 = document.getElementById("results1").textContent =
+    "Guess Higher!";
+  } else if (userGuess == compNumber) {
+    computerText = document.getElementById("results").textContent =
+      "That is correct!!";
+      let numRandom = Math.floor(Math.random() * 3 + 1);
+      if(numRandom == 1){
+        computerText1 = document.getElementById("results1").textContent =
+        "are you cheating?";
+      }else if(numRandom == 2){
+        computerText1 = document.getElementById("results1").textContent =
+        "Sheeeesh";
+      }else if(numRandom == 3){
+        computerText1 = document.getElementById("results1").textContent =
+        "THE FBI's ARE SHAKING!";
+      }
+ 
+    numberRandom = null;
+  }
+}
